7cd4086aac8d386ff2f69c6294527d407cd98fb1
MD ASHRAF ADIB <51002560+mdashrafadib@users.noreply.github.com>
2023-03-28 01:27:29 +0530

    Update index.js

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -5,7 +5,7 @@ var express = require('../../');
 var app = module.exports = express()
 
 app.get('/', function(req, res){
-  res.send('Hello PearlThoughts');
+  res.send('Updated version');
 });
 
 /* istanbul ignore next */
