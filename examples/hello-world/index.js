a2c1e8d4970bc95d522e44a6033e4684cb048ee9
MD ASHRAF ADIB <51002560+mdashrafadib@users.noreply.github.com>
2023-03-27 19:13:09 +0530

    Update index.js

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -5,7 +5,7 @@ var express = require('../../');
 var app = module.exports = express()
 
 app.get('/', function(req, res){
-  res.send('Skull Island');
+  res.send('Hello World');
 });
 
 /* istanbul ignore next */
